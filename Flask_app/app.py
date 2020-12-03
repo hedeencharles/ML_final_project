ddd828388ab49b5f0bd5712ec7a2d3b788ceb821
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-12-02 21:42:55 -0800

    started fleshing out app.py

---
+++ Flask_app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, url_for, request, render_template
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Landing page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Route that will receive input form data
+@app.route("/user/add", methods=["GET", "POST"]) # need to figure this line out
+def calc_charges():
+    if request.method == "POST":
+        charges = Charges(
+            age=request.form["age"],
+            gender=request.form["gender"],
+            bmi=request.form["bmi"],
+            children=request.form["children"],
+            smoker=request.form["smoker"],
+            region=request.form["region"],
+        )
+
+    # Redirect back to home page
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
