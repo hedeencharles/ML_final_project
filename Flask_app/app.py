54c828a45c47fc16872146a66e439804ab1d123d
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-12-03 23:24:47 -0800

    built out most of the script, need file name

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -13,21 +13,34 @@ app = Flask(__name__)
 def index():
     return flask.render_template("index.html")
 
+def ValuePredictor(predict_list):
+    to_predict = np.array(predict_list).reshape(1,6)
+    loaded_model = pickle.load(open("model.pkl","rb"))
+    calc_charges = loaded_model.predict(to_predict)
+    return calc_charges[0]
+
 # Route that will receive input form data
-@app.route("/user/add", methods=["GET", "POST"]) # need to figure this line out
+@app.route("/", methods=["GET", "POST"]) # need to figure this line out
 def calc_charges():
     if request.method == "POST":
-        charges = Charges(
-            age=request.form["age"],
-            gender=request.form["gender"],
-            bmi=request.form["bmi"],
-            children=request.form["children"],
-            smoker=request.form["smoker"],
-            region=request.form["region"],
-        )
+        predict_list = request.form.to_dict()
+        predict_list = list(predict_list.values())
+        # predict_list = list(map(float, to_predict_list))
+        calc_charges = ValuePredictor(predict_list)
+        prediction = float(calc_charges)
+
+
+        # charges = Charges(
+        #     age=request.form["age"],
+        #     gender=request.form["gender"],
+        #     bmi=request.form["bmi"],
+        #     children=request.form["children"],
+        #     smoker=request.form["smoker"],
+        #     region=request.form["region"],
+        # )
 
     # Redirect back to home page
-    return redirect(url_for('home'))
+    return redirect(url_for('home'), prediction=prediction)
 
 
 if __name__ == "__main__":
